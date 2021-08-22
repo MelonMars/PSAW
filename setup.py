9f2d4041cd20308ddcaee9bcf1fe0b5ffe3ee386
MelonMars <79235133+MelonMars@users.noreply.github.com>
2021-08-22 12:44:27 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'PSAW',         # How you named your package folder (MyLib)
   packages = ['PSAW'],   # Chose the same as "name"
-  version = '0.1',      
+  version = '0.2',      
   license='MIT',        
   description = 'A python api wrapper for scratch',   
   author = 'Carter Pfaff',                   
